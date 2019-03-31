a0489e59b1525fe20a539663049056ac22cf36bb
cfinn16 <connorfinnegan16@gmail.com>
2019-03-31 16:21:18 -0400

    login and logout working, fixed cities index image sizing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
-  # helper_method :current_user
-  #
-  # def current_user
-  #   @current_user = Player.find_by({ id: session[:user_id] })
-  # end
+  helper_method :current_user, :is_logged_in
+
+  def current_user
+    @current_user = Player.find_by({ id: session[:user_id] })
+  end
+
+  def is_logged_in
+    current_user != nil
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy', as: 'logout'
 
   post '/teams/:id/add_to_teams', to: 'teams#add_to_teams', as: "add_to_teams"
 
@@ -24,7 +24,5 @@ Rails.application.routes.draw do
   get "/players/current", to: "players#current_player", as: "current_player"
 
   resources :players, only: [:new, :create, :show]
-
-  # get "/sports", to: "sports#show", as: "eligible_sports"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
